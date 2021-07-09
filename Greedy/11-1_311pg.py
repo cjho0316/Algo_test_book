e5ac7b862120a837e98805368bc250f57f01b14e
cjho03160316 <>
2021-07-09 18:39:49 +0000

    그리디 실전문제

--- Greedy/11-1_311pg.py
+++ Greedy/11-1_311pg.py
@@ -2,7 +2,6 @@ N = int(input())
 K = list(map(int, input().split()))
 cnt = 0
 i = 0
-<<<<<<< HEAD
 result = 0
 
 K.sort()    #정렬
@@ -15,26 +14,26 @@ for i in K:
 
 
 print(result)
-=======
-K.sort()    #정렬
-K.reverse()
+# =======
+# K.sort()    #정렬
+# K.reverse()
 
-while True:
-  if i >= N+1:
-    break
+# while True:
+#   if i >= N+1:
+#     break
 
-  if K[i] == 1:
-    i += 1
+#   if K[i] == 1:
+#     i += 1
     
-  else:
-    i += K[i]
-    if i >= N+1:
-      break
+#   else:
+#     i += K[i]
+#     if i >= N+1:
+#       break
 
-  cnt += 1
+#   cnt += 1
 
-print(cnt)
->>>>>>> aa431fb43484b6c3c979c00b760aa4b3e6483b54
+# print(cnt)
+# >>>>>>> aa431fb43484b6c3c979c00b760aa4b3e6483b54
 
 # 공포도 X인 모험가 -> 반드시 X명 이상으로 구성한 모험가 그룹 참여
 # : 최댓값을 찾아서 그 그룹에다 때려박아야 하는일..
--- Greedy/11-3_313pg.py
+++ Greedy/11-3_313pg.py
@@ -15,26 +15,27 @@ for value in S:
 
 
 print(min(zero_cnt, one_cnt))
-=======
-cnt = 0
-i = 0
 
-if S.count(1) < S.count(0):
-  K = 1
-else:
-  K = 0
+# =======
+# cnt = 0
+# i = 0
 
-while True:
-  if i > len(S)-1:
-    break
+# if S.count(1) < S.count(0):
+#   K = 1
+# else:
+#   K = 0
 
-  if S[i] == K and S[i] != S[i+1]: 
-    cnt += 1
+# while True:
+#   if i > len(S)-1:
+#     break
 
-  i+=1
+#   if S[i] == K and S[i] != S[i+1]: 
+#     cnt += 1
 
-print(cnt)
->>>>>>> aa431fb43484b6c3c979c00b760aa4b3e6483b54
+#   i+=1
+
+# print(cnt)
+# >>>>>>> aa431fb43484b6c3c979c00b760aa4b3e6483b54
 
 
 # 0 과1주어져 있을 때 문자열 S의 모든 숫자를 전부 같게 만들기 
